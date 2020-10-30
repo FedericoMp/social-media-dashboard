a1af46bb148e0034778dd0bc18f9cc4fc706722c
Federico Perez <fperez@mobilecomputing.com.ar>
2020-10-30 18:21:12 -0300

    Updates

---
+++ precache-manifest.89b805f18053abd1a654a191c7269a6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de4ba220c0d6c38599082c0a2e2e743",
+    "url": "/FedericoMp/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "1c364651943282599d94",
+    "url": "/FedericoMp/social-media-dashboard/static/css/main.61866c29.chunk.css"
+  },
+  {
+    "revision": "ca18215063c178b5ef86",
+    "url": "/FedericoMp/social-media-dashboard/static/js/2.798a6963.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/FedericoMp/social-media-dashboard/static/js/2.798a6963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c364651943282599d94",
+    "url": "/FedericoMp/social-media-dashboard/static/js/main.752ff781.chunk.js"
+  },
+  {
+    "revision": "1b123d28843ecd0970dc",
+    "url": "/FedericoMp/social-media-dashboard/static/js/runtime-main.46535b25.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/FedericoMp/social-media-dashboard/static/media/icon-youtube.8499da12.svg"
+  }
+]);
